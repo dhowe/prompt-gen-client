0f550fd914115d952bc8c470d8b52d38a603d5e1
dhowe <dhowe@cims.nyu.edu>
2023-04-14 17:50:40 -0400

    initial

---
+++ socketio-test.py
@@ -0,0 +1,29 @@
+import socketio
+
+sio = socketio.Client()
+uid = 'dianeto@gmail.com'
+
+@sio.event
+def connect():
+    print(f'connected as {uid}...')
+
+
+@sio.event
+def on_publish(data):
+    print('/published: ', data)
+
+
+@sio.event
+def on_generate(data):
+    print('/generated: ', data)
+    # sio.emit('my response', {'response': 'my response'})
+
+
+@sio.event
+def disconnect():
+    print('...disconnected')
+
+
+sio.connect('ws://192.241.209.27:5050', auth={'uid': uid})
+#sio.connect('ws://localhost:5050', auth={'uid': uid}, wait_timeout=1)
+sio.wait()
