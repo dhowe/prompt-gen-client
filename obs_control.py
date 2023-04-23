b581ea58cda64e08616e75fc7225b65c10f383a3
dhowe <dhowe@cims.nyu.edu>
2023-04-23 14:44:43 -0400

    updates to client auth

--- obs_control.py
+++ obs_control.py
@@ -1,37 +1,39 @@
-import obsws_python as obs
 import PySimpleGUI as sg
-import inspect
-import time
-import os, sys
-import multiprocessing as mp
+import obsws_python as obs
+
 
 def debug(text):
     # make colorful and styled text
     print(f'\033[92m{text}\033[0m')
 
+
 cl = None
-    
+
+
 def _write_settings(ip, port, password):
     with open('obs_settings.txt', 'w') as f:
         f.write(f'{ip} {port} {password}')
+
 
 def _read_obs_settings():
     global cl, ip, port, password
     with open('obs_settings.txt', 'r') as f:
         ip, port, password = f.read().split(' ')
-        print("Connecting to OBS... on ip: ", ip, " port: ", port, " password: ", password)
+        print("Connecting to OBS... at ip:", ip, "port:", port, "password:", password)
         try:
             cl = obs.ReqClient(ip=ip, port=port, password=password)
             return "Connected to OBS at " + ip + ":" + port
         except ConnectionRefusedError:
             return "Failed to connect to OBS at " + ip + ":" + port
-    
+
+
 # read argument from command line
 _read_obs_settings()
 
 # Custom theme
 sg.theme("DarkTeal10")
 sg.set_options(font=("Helvetica", 17))
+
 
 class Scenes:
     def __init__(self):
@@ -53,8 +55,10 @@ class Scenes:
         else:
             return None
 
+
 def is_text(item):
     return 'text' in item['inputKind']
+
 
 def _connect_to_obs():
     try:
@@ -75,14 +79,17 @@ def _connect_to_obs():
 def toggle_mic():
     cl.toggle_input_mute('Mic/Aux')
 
+
 def show_items():
     cur_scene = cl.get_current_program_scene().current_program_scene_name
     items = cl.get_scene_item_list(cur_scene).scene_items
     return items
 
+
 def show_inputs():
     inputs = cl.get_input_list().inputs
     return inputs
+
 
 def show_texts():
     inputs = cl.get_input_list().inputs
@@ -111,28 +118,27 @@ class OBS:
         self.change_text(new, text)
         self.change_text(old, "")
         return f"Dialogue: {text}"
-    
+
     def split(self, text):
         words = text.split()
         lines = []
         current_line = ""
-        
+
         for word in words:
             if len(current_line + " " + word) <= 60:
                 current_line += " " + word
             else:
                 lines.append(current_line.strip())
                 current_line = word
-        
+
         lines.append(current_line.strip())
-        
+
         return "\n".join(lines), len(lines) > 1
 
-    
     def update_topic(self, new_topic):
         self.change_text(self.topic, new_topic.strip())
         return f"Topic: {new_topic}"
-    
+
     def update_obs_connection(self, ip, port, password):
         try:
             global cl
@@ -145,10 +151,9 @@ class OBS:
         window['connected'].update(message)
         return message
 
-
     def change_text(self, name, new_text):
         if not name:
-            return "Enter a text source. One of: " +  " ".join(show_texts())
+            return "Enter a text source. One of: " + " ".join(show_texts())
 
         try:
             settings = cl.get_input_settings(name).input_settings
@@ -157,26 +162,31 @@ class OBS:
             return f"'{name}' changed to '{new_text}'."
         except:
             msg = f"Failed to change '{name}' to '{new_text}'.\n"
-            msg += "Available text sources: " +  show_texts()
-        
+            msg += "Available text sources: " + show_texts()
+
 
 obs_state = OBS()
 
 scenes = Scenes()
 
+
 # Buttons
 def cycle_scenes():
     scenes.cycle()
 
+
 def send_subtitles(lines):
     return obs_state.change_subtitles(lines)
+
 
 def change_text(name, new_text):
     return obs_state.change_text(name, new_text)
 
+
 # Automatically generate buttons based on available functions
 function_buttons = []
-available_functions = [(name, func) for name, func in globals().items() if callable(func) and not name.startswith("_") and name != "update_output"]
+available_functions = [(name, func) for name, func in globals().items() if
+                       callable(func) and not name.startswith("_") and name != "update_output"]
 for name, func in available_functions:
     function_buttons.append(sg.Button(name, key=name, size=(12, 2), pad=((5, 5), (0, 5))))
 
@@ -191,16 +201,22 @@ layout = [
     [sg.Text("Field:", size=(0, 0)), sg.InputText("", key="field", size=(30, 1))],
     [sg.Text("Value:", size=(0, 0)), sg.Multiline("", key="value", size=(30, 1))],
     function_buttons[: len(function_buttons) // 2],
-    function_buttons[len(function_buttons) // 2 :],
+    function_buttons[len(function_buttons) // 2:],
     [sg.Button("Exit", key="exit", size=(12, 2), pad=((0, 0), (0, 5)))],
 ]
 
 window = sg.Window("OBS Control", layout)
 
+
 def update_output(window, content):
     if content:
         print("content", str(content))
         window["output"].update(str(content))
+
+
+def secret():
+    return password
+
 
 def event_loop():
     return
--- socketio-test.py
+++ socketio-test.py
@@ -1,24 +1,29 @@
+import threading
 import socketio
 import obs_control
-import threading
 
 sio = socketio.Client()
 uid = 'test@test.com'
-driver = uid
+dashboard_url = 'ws://192.241.209.27:5050' #'ws://localhost:5050'
+global_driver = uid
+
 
 @sio.event
 def connect():
-    print(f'connected as {uid}...')
+    print(f'Connecting to Dashboard... at {dashboard_url}, uid: {uid}')
+
 
 @sio.event
 def on_publish(data):
     print('/published: ', data)
     update_subtitles(data)
 
+
 @sio.event
 def on_generate(data):
     print('/generated: ', data)
     # sio.emit('my response', {'response': 'my response'})
+
 
 @sio.event
 def update_topic(data):
@@ -32,7 +37,8 @@ def update_topic(data):
         except Exception as e:
             message = str(e)
             print(message)
-    sio.emit('text_updated', {'updated': updated,'message': message, "field": field})
+    sio.emit('text_updated', {'updated': updated, 'message': message, "field": field})
+
 
 @sio.event
 def update_subtitles(data):
@@ -45,28 +51,31 @@ def update_subtitles(data):
         except Exception as e:
             message = str(e)
             print(message)
-    sio.emit('text_updated', {'updated': updated,'message': message, "field": "subtitles"})
+    sio.emit('text_updated', {'updated': updated, 'message': message, "field": "subtitles"})
+
 
 @sio.event
 def update_driver(data):
     # Change the username that has control over the stream.
     # All messages they send to the publish queue
     # will be displayed at the OBS instance
-    driver = data.get("driver", driver)
+    driver = data.get("driver", global_driver)
     print(f'new driver: {driver}')
 
+
 def authenticate_driver(data):
-    is_driver = (data == driver) or \
-        isinstance(data, dict) and data.get("author") == driver
-    return is_driver, f"{data.get('author')} is not the driver" if not is_driver else "" 
+    is_driver = (data == global_driver) or \
+                isinstance(data, dict) and data.get("author") == global_driver
+    return is_driver, f"{data.get('author')} is not the driver" if not is_driver else ""
+
 
 @sio.event
 def update_obs(data):
     connected = False
     try:
         message = obs_control.update_obs_connection(
-            data.get("ip"), 
-            data.get("port"), 
+            data.get("ip"),
+            data.get("port"),
             data.get("password")
         )
         connected = True
@@ -74,16 +83,19 @@ def update_obs(data):
         message = str(e)
 
     sio.emit('obs_connected', {'connected': connected, 'message': message})
-    
+
 
 @sio.event
 def disconnect():
     print('...disconnected')
 
+
 def listen():
-    sio.connect('ws://192.241.209.27:5050', auth={'uid': uid})
-    #sio.connect('ws://localhost:5050', auth={'uid': uid}, wait_timeout=1)
+    # DH: added some auth here
+    # sio.connect('ws://192.241.209.27:5050', auth={'uid': uid, 'secret': obs_control.secret()}, wait_timeout=1)
+    sio.connect(dashboard_url, auth={'uid': uid, 'secret': obs_control.secret()}, wait_timeout=1)
     sio.wait()
+
 
 if __name__ == '__main__':
     gui_thread = threading.Thread(target=obs_control.event_loop)
