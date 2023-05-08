82398c78e4961dbc4ab654ac830373f827d2e99e
dhowe <dhowe@cims.nyu.edu>
2023-05-07 23:20:10 -0400

    testing with scene_name

--- tests/socket_protocol_tests.py
+++ tests/socket_protocol_tests.py
@@ -6,11 +6,11 @@ import socketio
 
 socket_io = socketio.Client()
 
-# driver_uid = 'test@test.com'
-driver_uid = "alex.calderwood@tenderclaws.com"
+driver_uid = 'test@test.com'
+#driver_uid = "alex.calderwood@tenderclaws.com"
 
-# dashboard_url = 'ws://localhost:5000'
-dashboard_url = 'ws://192.241.209.27:5050/'  #
+dashboard_url = 'ws://localhost:5000'
+# dashboard_url = 'ws://192.241.209.27:5050/'  #
 
 responses = {
     'load_scene_recieved': False,
@@ -21,18 +21,22 @@ responses = {
 
 @socket_io.event
 def on_scene_loaded(packet):
-    print(f'got /on_scene_loaded')
+    print(f'got /on_scene_loaded scene_name={packet["data"]["scene_name"]}')
     responses['load_scene_recieved'] = True
 
 
 @socket_io.event
 def on_scene_complete(packet):
-    print(f'got /on_scene_complete')
+    print(f'got /on_scene_complete scene_name={packet["data"]["scene_name"]}')
     responses['end_scene_recieved'] = True
 
 
 @socket_io.event
 def on_error(packet):
+    '''
+    NOTE: server will send this event on serious error
+    It can only be overridden be sending a new load_scene msg
+    '''
     msg = packet["data"]["message"]
     print(f'got /on_error message="{msg}"')
 
@@ -65,7 +69,7 @@ if __name__ == "__main__":
         'secret': config['dashboard_password']
     }, wait_timeout=1)
 
-    socket_io.wait()
+    # socket_io.wait()
 
     # check that we're connected
     time.sleep(1)
@@ -74,27 +78,28 @@ if __name__ == "__main__":
         raise Exception(f'/connect failed with status={responses["on_connect"]}')
 
     # load json scene file
-    scene_json = open(os.path.dirname(__file__) + '/../test_scene.json').read()
-    socket_io.emit('load_scene', {'scene_json': scene_json})
-    print('sent /load_scene')
+    file_name = 'test_scene.json'
+    scene_json = open(os.path.dirname(__file__) + f'/../{file_name}').read()
+    socket_io.emit('load_scene', {'scene_json': scene_json, 'scene_name': 'test_scene.json'})
+    print(f'sent /load_scene {file_name}')
 
     # verify we've load scene file
     time.sleep(1)
     if not responses['load_scene_recieved']:
         socket_io.disconnect()
         raise Exception('/load_scene not recieved ')
+    if 1:
+        time.sleep(5)
 
-    time.sleep(5)
+        # end the scene
+        socket_io.emit('end_scene')
+        print('sent /end_scene')
 
-    # end the scene
-    socket_io.emit('end_scene')
-    print('sent /end_scene')
-
-    # verify the scene is ended
-    time.sleep(1)
-    if not responses['end_scene_recieved']:
-        socket_io.disconnect()
-        raise Exception('/end_scene not recieved ')
+        # verify the scene is ended
+        time.sleep(1)
+        if not responses['end_scene_recieved']:
+            socket_io.disconnect()
+            raise Exception('/end_scene not recieved ')
 
 time.sleep(5)
 socket_io.disconnect()
